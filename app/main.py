694a8a0ecfc4cf790939d3e1753512f4f030ac63
andrelucasf <andre.lucas.lemos@hotmail.com>
2022-07-11 15:55:20 -0300

    Adicionando frontend e configurando CORS na API
    Co-authored-by: Lucas Medeiros <medeirosrosalucas@gmail.com>

--- app/main.py
+++ app/main.py
@@ -5,8 +5,21 @@ from draw_graph import draw_graph
 
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class Graph(BaseModel):
